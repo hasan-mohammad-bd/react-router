e184a919e6edf9c9a01e8f67a9ded2ab5cf9fe4b
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-29 11:11:03 +0600

    react route recap

--- src/note.js
+++ src/note.js
@@ -11,5 +11,8 @@ installation process
 ==============================
 1. npm install react-router-dom@6
 2. go to index.js > copy the component of "Browser Router".
-3. 
+3. set routes path in app.js (use not found page as well)
+4. to dynamically navigate through use "useNavigate"
+5. set header (if we write anything above routes, the text will be same for all routes.)   
+6. use link instead of using <a/> which will make the page stand still(no reload)
 */
